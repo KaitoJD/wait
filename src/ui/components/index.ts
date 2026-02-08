43a8b08e7bd98573c9910c1f7c0d6057dbd566fd
KaitoJD <nguyensynguyen7856@gmail.com>
2026-02-08 20:45:39 +0700

    feat: export ForecastDisplay component in index file

--- src/ui/components/index.ts
+++ src/ui/components/index.ts
@@ -1,5 +1,6 @@
 export { Header } from './header';
 export { Menu } from './menu';
 export { WeatherDisplay } from './weather-display';
+export { ForecastDisplay } from './forecast-display';
 export { LocationInput } from './location-input';
 export { StatusBar } from './status-bar';
