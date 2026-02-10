1657e1185eaa9b392f4c6bce53f07ff0ee57119f
KaitoJD <nguyensynguyen7856@gmail.com>
2026-02-10 23:57:26 +0700

    feat: re-add SettingsDisplay component to index exports

--- src/ui/components/index.ts
+++ src/ui/components/index.ts
@@ -2,5 +2,6 @@ export { Header } from './header';
 export { Menu } from './menu';
 export { WeatherDisplay } from './weather-display';
 export { ForecastDisplay } from './forecast-display';
+export { SettingsDisplay } from './settings-display';
 export { LocationInput } from './location-input';
 export { StatusBar } from './status-bar';
